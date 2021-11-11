c4ea561ded9ca04afc9bc6ecf0b16b7ffd31f18a
Amalia <amalialorenaferariu@gmail.com>
2021-11-11 14:51:38 +0100

    init

---
+++ js/myscript.js
@@ -0,0 +1,47 @@
+var carousel = new Vue (
+{
+    el: "#carousel",
+
+    data: {
+        slides: [
+            {
+            image: 'img/01.jpg',
+            title: 'Svezia',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+            image: 'img/02.jpg',
+            title: 'Svizzera',
+            text: 'Lorem ipsum, dolor sit amet.',
+            },
+            {
+            image: 'img/03.jpg',
+            title: 'Gran Bretagna',
+            text: 'Lorem ipsum, dolor sit ametEt temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi.',
+            },
+            {
+            image: 'img/04.jpg',
+            title: 'Germania',
+            text: 'Lorem ipsum, dolor sit ametEt temporibus.',
+            },
+            {
+            image: 'img/05.jpg',
+            title: 'Paradise',
+            text: 'Lorem ipsum, dolor sit ametEt temporibus voluptatum suscipit.',
+            },
+            
+
+        ],
+
+    },
+
+});
+
+
+
+
+
+
+//Rifare l’esercizio dello slider come fatto assieme in classe.
+//Quindi farlo funzionare con Vue, nel modo del framework.
+
